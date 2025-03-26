17a7f438f79c9d5e607c36c666576aa5d57ef269
AlexCode-dot <alle7000.andersson@gmail.com>
2025-03-26 15:19:46 +0100

    - changed vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    manifest: true, // Aktiverar manifestfilen
+    outDir: 'static/dist', // Sätt utmatningsmappen till "dist"
+    rollupOptions: {
+      input: 'src/react/main.jsx', // Startpunkt för appen
+    },
+    emptyOutDir: false, // Förhindrar att static/script rensas varje build
+  },
 })
