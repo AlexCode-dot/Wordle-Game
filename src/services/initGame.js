54eeea2900610e91a4826bd39787170548c610d0
AlexCode-dot <alle7000.andersson@gmail.com>
2025-03-27 22:09:42 +0100

    - added css for the basic site structure

--- src/services/initGame.js
+++ src/services/initGame.js
@@ -3,10 +3,11 @@ import getWord from './wordSelector.js'
 export default async function initGame(api, wordLength, noLetterDuplicate) {
   const wordList = await api.loadWords()
   const correctWord = getWord(wordList, wordLength, noLetterDuplicate)
-  if (correctWord == 'No matching word found.') {
-    return correctWord
+
+  if (correctWord === 'No matching word found.') {
+    return { message: correctWord, gameStarted: false }
   } else {
     console.log(correctWord)
-    return correctWord.length
+    return { wordLength: correctWord.length, gameStarted: true }
   }
 }
